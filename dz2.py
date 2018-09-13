a8c4f814d8193f6686955a4407dd523efa41c162
Andrew <Andrew>
2018-09-13 17:06:18 +0300

    added

---
+++ dz2.py
@@ -0,0 +1,25 @@
+
+#Task 1
+
+a = int(input("Enter a number: "))
+c = input("choose into (b) or into (k): ")
+
+if c == 'b':
+    print("%d Байт = %d Килобайт" % (a, a*1024))
+elif c == 'k':
+    print("%d Килобайт = %d Байт" % (a, a/1024))
+
+
+#Task 2
+
+n = int(input("Enter a number: "))
+s = 0
+m = 1
+while n > 0:
+    s += n%10
+    m *= n%10
+    n = n//10
+
+print("Summ: ", s)
+print ("Multiply", m)
+
