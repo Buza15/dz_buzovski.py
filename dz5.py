963f63f46fff289eeacb2dd0b7f4632fef1187a5
Andrew <Andrew>
2018-09-14 16:35:00 +0300

    last

---
+++ dz5.py
@@ -0,0 +1,57 @@
+
+#Task 1
+
+a = int(input("Enter a number: "))
+c = input("choose into (b) or into (k): ")
+
+if c == 'b':
+    print("%d Байт = %d Килобайт" % (a, a*1024))
+elif c == 'k':
+    print("%d Килобайт = %d Байт" % (a, a/1024))
+
+
+#Task 2
+
+n = int(input("Enter a number: "))
+s = 0
+m = 1
+while n > 0:
+    s += n%10
+    m *= n%10
+    n = n//10
+
+print("Summ: ", s)
+print ("Multiply", m)
+
+
+
+
+#3 task
+start = input()
+end = input()
+step = input()
+
+for i in range(start, end, step):
+    print(-1.24 * i ** 2 + i)
+
+# 4 task
+
+value = input("Type number")
+
+for i in range(len(value) / 2):
+    if value[i] != value[-(i+1)]:
+        print('Not palindrome')
+        break
+else:
+    print('Palindrome')
+
+# 5 task
+a = [1, 2, -3]
+
+_sum = 0
+_count = 0
+for value in a:
+    if value > 0:
+        _sum += value
+        _count += 1
+print(_sum / (_count or 1))
